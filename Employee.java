16b4a5dd34345d75b92f2fd1271cd96a7cd4a782
Wasfi Sarrar <61529134+wsarrar@users.noreply.github.com>
2020-07-18 11:05:41 -0700

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,9 +1,11 @@
 
-public class Employee extends People {
+public class Employee extends People 
+{
 	protected int payScale;
 	
 	public Employee(String fName, String lName, String pNum, double pR, int pS) 
 	{
+		// Calls constructor from the parent class (People class)
 		super(fName, lName, pNum);
 		this.payScale = pS;
 	}
